9b4e68806656cb4c1bb21d9ca1c099eaee5ab932
Pedro Cezar <pedrofcezar@gmail.com>
2018-11-07 14:38:45 -0200

    crud funcionando 100%"

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.all
@@ -21,8 +22,30 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    @article.update(article_params)
+    if @article.save
+      redirect_to article_path(@article), message: "Aubriagdow!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path, message: "Artigo #{@article.title} foi excluído!"
+  end
 
 private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
       params.require(:article).permit(
         :author,
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ApplicationRecord
+
   validates :author, presence: true
   validates :title, presence: true, uniqueness: true
   validates :content, presence: true, uniqueness: true
+
   validates :dog_key, inclusion: { in: [ true, false ] }
   validates :cat_key, inclusion: { in: [ true, false ] }
   validates :pet_key, inclusion: { in: [ true, false ] }
