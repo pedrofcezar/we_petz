c87b7c020c58b03964dde0fc55d3cde5c10bd3d7
Pedro Cezar <pedrofcezar@gmail.com>
2018-11-08 15:48:46 -0200

    small changes articles views

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :article
   validates :name, presence: true
   validates :stars, presence: true, inclusion: { in: [1..5] }
-  validates :review_content, presence: true, lenght: { minumum: 15, maximum: 240 }
+  validates :review_content, presence: true
 end
