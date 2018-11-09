aa6b13df4fe403d1d64ce87a300a2d49fede4db4
Pedro Cezar <pedrofcezar@gmail.com>
2018-11-09 14:16:55 -0200

    models review

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,4 @@ class Review < ApplicationRecord
   validates :name, presence: true
   validates :stars, presence: true, inclusion: { in: [1...5] }
   validates :review_content, presence: true
-
 end
