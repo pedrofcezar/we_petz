465821443f36ae073898e191cf3a4828a2992f9b
Pedro Cezar <pedrofcezar@gmail.com>
2018-11-08 23:02:09 -0200

    fixed review create

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :article
   validates :name, presence: true
-  validates :stars, presence: true, inclusion: { in: [1..5] }
+  validates :stars, presence: true, inclusion: { in: [1...5] }
   validates :review_content, presence: true
 end
